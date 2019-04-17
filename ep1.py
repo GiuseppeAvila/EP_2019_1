2acb155d3d83aad51ddce71f19fceee0cbbea7cd
Daniel-Terra <danielgt1@al.insper.edu.br>
2019-04-17 17:41:13 -0300

    Coloquei o código de jonson

--- ep1.py
+++ ep1.py
@@ -8,41 +8,13 @@
 
 '''
 
+
+import json
 import supreme_weapons as sw
 
 def carregar_cenarios():
-    cenarios = {
-        "inicio": {
-            "titulo": "Saguao do perigo",
-            "descricao": "Voce esta no saguao de entrada do insper",
-            "opcoes": {
-                "andar professor": "Tomar o elevador para o andar do professor",
-                "biblioteca": "Ir para a biblioteca"
-            }
-        },
-        "andar professor": {
-            "titulo": "Andar do desespero",
-            "descricao": "Voce chegou ao andar da sala do seu professor",
-            "opcoes": {
-                "inicio": "Tomar o elevador para o saguao de entrada",
-                "professor": "Falar com o professor"
-            }
-        },
-        "professor": {
-            "titulo": "O monstro do Python",
-            "descricao": "Voce foi pedir para o professor adiar o EP. "
-                         "O professor revelou que é um monstro disfarçado "
-                         "e devorou sua alma.",
-            "opcoes": {}
-        },
-        "biblioteca": {
-            "titulo": "Caverna da tranquilidade",
-            "descricao": "Voce esta na biblioteca",
-            "opcoes": {
-                "inicio": "Voltar para o saguao de entrada"
-            }
-        }
-    }
+    with open("cenarios.json","r") as arquivo_cenarios:
+        cenarios = json.load(arquivo_cenarios)
     nome_cenario_atual = "inicio"
     return cenarios, nome_cenario_atual
 
