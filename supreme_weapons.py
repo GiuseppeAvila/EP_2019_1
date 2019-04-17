a0d52c5361487f130db6b5eef181d6926d4942d5
Daniel-Terra <danielgt1@al.insper.edu.br>
2019-04-17 16:08:24 -0300

    Alterei as armas

--- supreme_weapons.py
+++ supreme_weapons.py
@@ -1,3 +1,15 @@
-def teleporte():
+def TP():
     sala = "sala"
     return sala
+
+def DIM(dano_fisico,defesa):
+    multiplicador = 0
+    buff = 0
+    dano_fisico *= multiplicador
+    defesa += buff
+    return [dano_fisico,defesa]
+
+def SCOT(dano_magico):
+    dano_adicional = 0
+    dano_magico += dano_adicional
+    return dano_magico
