880da409de84b66abb1c1fcab5f1ec034038d350
Daniel-Terra <danielgt1@al.insper.edu.br>
2019-04-18 09:53:56 -0300

    Adicionei garagem, saboroso, rua em frente ao insper

--- cenarios.py
+++ cenarios.py
@@ -5,16 +5,14 @@
             "descricao": "Voce esta no saguao de entrada do insper",
             "opcoes": {
                 "andar professor": "Tomar o elevador para o andar do professor",
-                "biblioteca": "Ir para a biblioteca"
-            }
+                "biblioteca": "Ir para a biblioteca"}
         },
         "andar professor": {
             "titulo": "Andar do desespero",
             "descricao": "Voce chegou ao andar da sala do seu professor",
             "opcoes": {
                 "inicio": "Tomar o elevador para o saguao de entrada",
-                "professor": "Falar com o professor"
-            }
+                "professor": "Falar com o professor"}
         },
         "professor": {
             "titulo": "O monstro do Python",
@@ -26,20 +24,31 @@
         "biblioteca": {
             "titulo": "Caverna da tranquilidade",
             "descricao": "Voce esta na biblioteca",
-            "opcoes": {
-                "inicio": "Voltar para o saguao de entrada"
-                "fundo": "Avançar para os fundos da biblioteca"
-            },
+            "opcoes": {"inicio": "Voltar para o saguao de entrada", "fundo": "Avançar para os fundos da biblioteca"}
+        },
         "garagem": {
-        "titulo": "Abismo, o vazio e a escuridão",
-        "descricao": "Voce esta na garagem",
-        "opcoes": {
-                "inicio": "Voltar para o saguao de entrada"
-                }
-            }
+                'título': 'Abismo, o vazio e a escuridão', 
+                'descrição': 'Você está na garagem...', 
+                'opcoes': {'subir elevador': 'Voltar para a superfície', 
+                           'explorar': 'Ir mais ao fundo', 
+                           'corredor': 'Seguir em direção aos laboratórios e ao baja'}
+        },
+        'rua em frente ao Insper': {
+                'título': 'Os portões', 
+                'descrição': 'Você saiu do Insper, está na rua', 
+                'opcoes': {'predio novo': 'Entrar no prédio novo', 
+                           'predio velho': 'Entrar no prédio velho', 
+                           'direita': 'Seguir a rua para a direita'}
+        },
+        'saboroso': {
+                'título': 'El flavors', 
+                'descrição': 'Você está no restaurante da esquina', 
+                'opcoes': {'comer': 'Gastar R$19.9999999999999999... e recuperar X de HP!', 
+                           'beber agua': 'Retornar sua adrenalina em X%! Apenas R$3!', 
+                           'conversar com Daniel Guzzo': 'Daniel?'}
         }
-        
     }
+
     nome_cenario_atual = "inicio"
     return cenarios, nome_cenario_atual
 
@@ -59,3 +68,17 @@ def situacao():
         "adiamento do EP (boa sorte...)")
     print()
             
+
+
+
+
+
+'''
+        "garagem": {
+                'título': 'Abismo, o vazio e a escuridão', 
+                'descrição': 'Você está na garagem...', 
+                'opcoes': {'subir elevador': 'Voltar para a superfície', 
+                           'explorar': 'Ir mais ao fundo', 
+                           'corredor': 'Seguir em direção aos laboratórios e ao baja'
+                           }}
+'''
