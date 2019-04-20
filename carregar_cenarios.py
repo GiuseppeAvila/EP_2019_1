076054e60767d5c409e9c71d2a3d32ac19c0890f
GiuseppeAvila <fernando.peppe@gmail.com>
2019-04-20 17:52:43 -0300

    Mudança de nome do arquivo cenarios.py para carregar_cenarios.py, update do código

---
+++ carregar_cenarios.py
@@ -0,0 +1,17 @@
+﻿
+import json
+
+def carregar_cenarios():
+    with open("cenarios.json", "r") as arquivo:
+        cenarios = json.loads(arquivo)
+    nome_cenario_atual = "inicio"
+    return cenarios, nome_cenario_atual
+
+def inicio(cenario_atual):
+    print(cenario_atual["título"])
+    print(cenario_atual["descrição"])
+
+def situacao():
+    with open("NA HORA DO SUFOCO.txt", "r",encoding="utf8") as arquivo:
+        conteudo = arquivo.read()
+    print(conteudo)
