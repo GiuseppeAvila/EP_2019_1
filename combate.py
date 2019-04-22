4ec91156b4e586f5778d770ed7ef259bdea7ada9
Daniel-Terra <danielgt1@al.insper.edu.br>
2019-04-22 18:42:06 -0300

    Fenomenal o combate está implementadíssissíssimo

--- combate.py
+++ combate.py
@@ -3,16 +3,11 @@ import random
 import json
 from pprint import pprint
 
-#def lutar():
-    
-    
-    
 def fugir(personagem, opcoes):
     print("Só corre!")
     
     AGI = personagem["Status"]["AGI"]
-    #numero = random.randint(0, 100)
-    numero = 1
+    numero = random.randint(0, 100)
     
     if numero >= (20 - AGI):
         print("Toma essa monstrengo! Você escapou!")
@@ -188,9 +183,11 @@ def A(personagem,monstro_atual,opcoes):
         monstro_vivo = True
                                           
     print("Você deu incríveis {} de dano!".format(dano_recebido))
+    print()
     
     if monstro_atual["HP_atual"] > 0:
         print("A vida do monstro atualmente é: {}".format(monstro_atual["HP_atual"]))
+        print()
                                 
     if not monstro_vivo:
         print()
@@ -244,9 +241,11 @@ def P(personagem,monstro_atual,opcoes):
         monstro_vivo = True
                                             
     print("Você deu incríveis {} de dano!".format(dano_recebido))
+    print()
             
     if monstro_atual["HP_atual"] > 0:
         print("A vida do monstro atualmente é: {}".format(monstro_atual["HP_atual"]))
+        print()
                                 
     if not monstro_vivo:
         print()
@@ -257,4 +256,179 @@ def P(personagem,monstro_atual,opcoes):
         print()
                     
     return monstro_vivo
-                       
+                     
+
+
+def discucao_fisica(personagem,monstro_atual,opcoes,i): 
+    
+    monstro_vivo = True
+    
+    # Escolha de batalha
+    print("[L]utar / [F]ugir ")
+    escolha_bat = input()
+    escolha_bat = escolha_bat.upper()
+                
+    #  Personagem Lutando!
+    if escolha_bat == "L":
+                    
+        #em_batalha = combate.lutar(personagem) 
+        print()
+        print("Take your time:")
+        print("[A]tacar / [P]sy / [I]tem / [S]tatus")
+
+        acao = input()
+        acao = acao.upper()
+        acoes_possiveis = ["A","P","I","S"]
+        print()
+
+        menu_atk = True
+        
+        while menu_atk:
+                        
+            if acao in acoes_possiveis:
+                
+                # ESCOLHA A
+                if acao == "A":
+                    monstro_vivo = A(personagem,monstro_atual,opcoes)
+                    menu_atk = False
+
+                # ESCOLHA S
+                elif acao == "S":
+                    pprint(personagem)
+                    print()
+                    print("Take your time:")
+                    print("[A]tacar / [P]sy / [I]tem / [S]tatus")
+                
+                    acao = input()
+                    acao = acao.upper()
+                    print()
+                    menu_atk = True
+                
+                # ESCOLHA P
+                elif acao == "P":
+                                
+                    if personagem["Status"]["PP_Atual"] >= 5:
+                        monstro_vivo = P(personagem,monstro_atual,opcoes)
+                        menu_atk = False
+                                
+                    else:
+                        print("Seu PP está baixo, precisa-se de 5PP para realizar um PP ATK!")
+                        print()
+                        print("Take your time:")
+                        print("[A]tacar / [P]sy / [I]tem / [S]tatus")
+                    
+                        acao = input()
+                        acao = acao.upper()
+                        print()
+                        menu_atk = True
+                
+                # ESCOLHA I
+                elif acao == "I":
+                    menu_atk = I(personagem)
+                
+            # ERRO DE ESCRITA
+            else:
+                print("Você ataca com {}! É super efetivo!".format(acao))
+                print()
+                print()
+                print("Com isso você causa fenomenais 0 de dano, já que {} não é uma opção válida".format(acao))
+                print("Para de bancar o espertinho e aprenda a jogar")
+                print()
+                print("Escolha uma opção abaixo.. e lembre-se, tome o tempo que precisar!")
+                print()
+                print("Take your time:")
+                print("[A]tacar / [P]sy / [I]tem / [S]tatus")
+                    
+                acao = input()
+                acao = acao.upper()
+                print()
+                menu_atk = True
+        
+    #  Fuga da personagem quando em batalha
+    elif escolha_bat == "F":
+        if i > 0:
+            print("Você já tentou fugir e não deu, agora se vira, acaba com ele")
+            
+        else:
+            monstro_vivo = fugir(personagem, opcoes)
+            i+=1
+            
+    #  Caso o monstro tenha morrido
+    elif monstro_atual["HP_atual"] <= 0:
+        monstro_vivo = False
+            
+    # Caso alguém escreva algo que n seja L ou F
+    else:
+        print("Não seja covarde!")
+        monstro_vivo = True
+                
+    return monstro_vivo  
+
+def monstrengo(monstro_atual,personagem): 
+    
+    n_morreu = True
+
+    acoes_possiveis = ["A","P"]
+    escolha_monstro = random.randint(0,1)
+    acao = acoes_possiveis[escolha_monstro]
+                
+    # ESCOLHA A
+    if acao == "A":
+        
+        F_ATK_monstro = monstro_atual["F_ATK"]
+    
+        F_DEF_personagem = personagem["Status"]["F_DEF"]
+    
+        dano_recebido = F_ATK_monstro - F_DEF_personagem*0.3
+    
+        if dano_recebido < 0:
+            dano_recebido = 0
+        
+        personagem["Status"]["HP_Atual"] -= dano_recebido
+        
+        print("Você recebeu infelizes {} de dano!".format(dano_recebido))
+    
+        if personagem["Status"]["HP_Atual"] > 0:
+            print("A sua vida atualmente é: {}, cuidado...".format(personagem["Status"]["HP_Atual"]))
+                
+    # ESCOLHA P
+    elif acao == "P":
+                                
+        if monstro_atual["PP_Atual"] >= 5:
+            
+            P_ATK_monstro = monstro_atual["P_ATK"]
+        
+            P_DEF_personagem = personagem["Status"]["P_DEF"]
+        
+            dano_recebido = P_ATK_monstro - P_DEF_personagem*0.3
+        
+            if dano_recebido < 0:
+                dano_recebido = 0
+            
+            monstro_atual["PP_Atual"] -= 5
+        
+            if monstro_atual["PP_Atual"] < 0:
+                monstro_atual["PP_Atual"] = 0
+            
+            personagem["Status"]["HP_Atual"] -= dano_recebido
+            
+            if personagem["Status"]["HP_Atual"] <= 0:
+                personagem["Status"]["HP_Atual"] = 0
+            
+                                            
+            print("Você recebeu infelizes {} de dano!".format(dano_recebido))
+            print()
+            
+            if personagem["Status"]["HP_Atual"] > 0:
+                print("A sua vida atualmente é: {}, cuidado...".format(personagem["Status"]["HP_Atual"]))
+                print()
+                                
+        else:
+            print("O monstro não conseguiu atacar... que sem graça!")
+            
+    # Caso o personagem tenha morrido
+    elif personagem["HP_atual"] <= 0:
+        n_morreu = False
+                
+    return n_morreu  
+
